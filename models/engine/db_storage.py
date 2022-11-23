8006fee3a433074f4b1c3369c00bb3c8ab023ff6
Facub10 <facub1011@gmail.com>
2022-11-23 12:45:22 -0300

    task 6 update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,6 +2,7 @@
 """This module defines a class to manage the database for hbnb clone"""
 from sqlalchemy import create_engine, MetaData
 import os
+from sqlalchemy.orm import sessionmaker
 
 
 class DBStorage:
@@ -13,12 +14,19 @@ class DBStorage:
         pwd = os.getenv('HBNB_MYSQL_PWD')
         host = os.getenv('HBNB_MYSQL_HOST')
         db = os.getenv('HBNB_MYSQL_DB')
-        engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                               .format(user, pwd, host, db), pool_pre_ping=True)
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, pwd, host, db), pool_pre_ping=True)
         if os.getenv('HBNB_ENV') == 'test':
             metadata_obj = MetaData()
             for t in metadata_obj:
-                t.drop(engine, checkfirst=False)
+                t.drop(self.__engine, checkfirst=False)
 
     def all(self, cls=None):
-        return
+        fs_dict = {}
+        if cls:
+            for data in self.__session.query:
+                if cls == data.__class__.__name__:
+                    fs_dict[f"{cls}.{data.id}"] = data
+        else:
+            for data in self.__session.query:
+                fs_dict[f"{cls}.{data.id}"] = data
