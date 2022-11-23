021f4b6a85797e6329e8a186750d442a817c5037
Facub10 <facub1011@gmail.com>
2022-11-23 11:37:04 -0300

    updating task6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
+from models.engine.file_storage import FileStorage
+from sqlalchemy import create_engine
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __talbename__ = "states"
+    name = Column(String(128), nullable=False)
+    user = os.getenv('HBNB_MYSQL_USER')
+    pwd = os.getenv('HBNB_MYSQL_PWD')
+    host = os.getenv('HBNB_MYSQL_HOST')
+    db = os.getenv('HBNB_MYSQL_DB')
+    engine = create_engine('mysql://{}:{}@{}/{}'.format(user, pwd, host, db))
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship("City", backref='State')
+    else:
+        @property
+        def cities(self):
+            l_cities = []
+            for city in FileStorage.all("City"):
+                if self.id == city.state_id:
+                    l_cities.append = city
+            return l_cities
