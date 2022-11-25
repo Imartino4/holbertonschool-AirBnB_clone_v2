9e4c1084952f687dad430e2e6e2a4dd018865c41
Facub10 <facub1011@gmail.com>
2022-11-25 14:23:44 -0300

    fixing city and base_model

--- models/city.py
+++ models/city.py
@@ -10,11 +10,6 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
 
     __tablename__ = "cities"
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", backref='cities')
-
-    else:
-        state_id = ""
-        name = ""
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", backref='cities')
