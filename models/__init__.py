8a4f7253732de32b29331cf988c140eeb9e01769
Imartino4 <ignacio.martacos@gmail.com>
2022-11-24 23:31:20 -0300

    checking task 5 without switch

--- models/__init__.py
+++ models/__init__.py
@@ -4,10 +4,11 @@ from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 import os
 
+"""
 if os.getenv('HBNB_MYSQL_DB') == 'db':
     storage = DBStorage()
     storage.reload()
 
-else:
-    storage = FileStorage()
-    storage.reload()
+else:"""
+storage = FileStorage()
+storage.reload()
