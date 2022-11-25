8762ba1ba5a33a73aed19fc24a5fe0b3679680c5
Imartino4 <ignacio.martacos@gmail.com>
2022-11-25 08:52:12 -0300

    switch in place

--- models/place.py
+++ models/place.py
@@ -7,35 +7,34 @@ from sqlalchemy.orm import relationship
 
 
 # Many to many relationship - esta es la tabla que asocia las dos clases
-place_amenity = Table('place_amenity', Base.metadata,
-                    Column(
-                        'place_id', String(60), ForeignKey(
-                        'places.id'), primary_key=True
-                        ),
-                    Column(
-                        'amenity_id', String(60),
-                        ForeignKey("ameinities.id"),
-                        primary_key=True, nullable=False
-                    ))
+if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                        Column('place_id', String(60), ForeignKey(
+                            'places.id'), primary_key=True),
+                        Column('amenity_id', String(60),
+                            ForeignKey("ameinities.id"),
+                            primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(Integer, default=0)
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
-    reviews = relationship("Review", backref='place')
-    amenities = relationship("Amenity", secondary=place_amenity,
-                                  viewonly=False)
-
+    
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        __tablename__ = 'places'
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=False)
+        number_rooms = Column(Integer, default=0)
+        number_bathrooms = Column(Integer, default=0)
+        max_guest = Column(Integer, default=0)
+        price_by_night = Column(Integer, default=0)
+        latitude = Column(Float, nullable=False)
+        longitude = Column(Float, nullable=False)
+        reviews = relationship("Review", backref='place')
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                    viewonly=False)
+
+    else:
         @property
         def reviews(self):
             """Returns a list of Reviews instances 
