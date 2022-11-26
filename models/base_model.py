f5fafda62cafd30fbd3a8d73522462fb4a6c1e6a
Imartino4 <ignacio.martacos@gmail.com>
2022-11-26 10:25:48 -0300

    change manage of kwargs on basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -26,12 +26,9 @@ class BaseModel:
             for k, v in kwargs.items():
                 if k!= '__class__':
                     setattr(self, k, v)
-            if 'id' not in kwargs.keys():
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs.keys():
-                self.created_at = datetime.now()
-            if 'updated_at' not in kwargs.keys():
-                self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             self.save()
 
     def __str__(self):
