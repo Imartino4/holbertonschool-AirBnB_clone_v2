142839943f8aa7575c300586ef831de1798dfc16
Facub10 <facub1011@gmail.com>
2022-11-26 16:33:47 -0300

    small fix

--- models/place.py
+++ models/place.py
@@ -14,6 +14,7 @@ place_amenity = Table('place_amenity', Base.metadata,
                                 primary_key=True, nullable=False))
 """
 
+
 class Place(BaseModel, Base):
     """ A place to stay """
 
@@ -21,13 +22,13 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, default=0)
     number_bathrooms = Column(Integer, default=0)
     max_guest = Column(Integer, default=0)
     price_by_night = Column(Integer, default=0)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     """"
     reviews = relationship("Review", backref='place', cascade='delete')
     amenities = relationship("Amenity", secondary="place_amenity",
