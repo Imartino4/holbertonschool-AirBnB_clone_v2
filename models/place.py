f44c602d771110252aa00a0aa5c04463819dce7d
Facub10 <facub1011@gmail.com>
2022-11-28 14:49:52 -0300

    fix amenities relationship

--- models/place.py
+++ models/place.py
@@ -31,10 +31,10 @@ class Place(BaseModel, Base):
     if (os.getenv('HBNB_TYPE_STORAGE') == 'db'):
         reviews = relationship(
             "Review", backref='place', cascade='all, delete')
-        amenities = relationship("Amenity",
-                                 secondary='place_amenity',
-                                 viewonly=False,
-                                 overlaps=True)
+        # amenities = relationship("Amenity",
+        #                          secondary='place_amenity',
+        #                          viewonly=False,
+        #                          overlaps=True)
 
     else:
         @property
