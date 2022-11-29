0f3db0ea173c69efdbc082d30895288814846479
Imartino4 <ignacio.martacos@gmail.com>
2022-11-29 15:12:15 +0000

    Task 3,4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This modules start a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    """Hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hello_hbnb2():
+    """HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def show_C(text):
+    """Show text"""
+    t = text.replace("_", " ")
+    return "C {}".format(t)
+
+
+@app.route("/python/<text>")
+def show_python(text="is_cool"):
+    """Show text python"""
+    t = text.replace("_", " ")
+    return "Python {}".format(t)
+
+
+@app.route("/number/<n>")
+def show_number(n):
+    """Show number"""
+    if n.isnumeric():
+        return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
