4694e4bd3987c475cd6efedc1fb40c8a12497fe0
Imartino4 <ignacio.martacos@gmail.com>
2022-11-30 21:47:55 -0300

    T7 - switch state dbs and fs

--- models/state.py
+++ models/state.py
@@ -11,15 +11,17 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref='state')
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref='state')
 
-    @property
-    def cities(self):
-        """Getter method to return cities
-        instances with current state_id"""
-        from models import storage
-        l_cities = []
-        for city in storage.all("City"):
-            if self.id == city.state_id:
-                l_cities.append(city)
-        return l_cities
+    else:
+        @property
+        def cities(self):
+            """Getter method to return cities
+            instances with current state_id"""
+            from models import storage
+            l_cities = []
+            for city in storage.all("City"):
+                if self.id == city.state_id:
+                    l_cities.append(city)
+            return l_cities
