2a2375ec0ffec5dcac8e55bae458e7d595083699
Imartino4 <ignacio.martacos@gmail.com>
2022-12-02 18:22:54 +0000

    T9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This modules start a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/cities_by_states")
+def show_html():
+    """Display states list in order"""
+    from models.state import State
+    from models.city import City
+
+
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
+    return render_template(
+        '8-cities_by_states.html', states=states, cities=cities)
+
+
+@app.teardown_appcontext
+def teardown(error):
+    """ """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
