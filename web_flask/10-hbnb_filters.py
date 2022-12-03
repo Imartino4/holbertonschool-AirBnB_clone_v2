9596ff2a7e5bafb34e8d99ec53a5816fe00defb4
Imartino4 <ignacio.martacos@gmail.com>
2022-12-03 19:11:44 +0000

    T11 - update htmls and task

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -8,30 +8,20 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route("/states")
-@app.route("/states/<id>")
+@app.route("/hbnb_filter")
 def show_html(id="None"):
-    """Display states list in order
-    *check_id = 1 when id is correct
-    """
+    """Display hbnb web page"""
     from models.state import State
     from models.city import City
+    from models.amenity import Amenity
 
     states = storage.all(State).values()
     cities = storage.all(City).values()
-    check_id = 0
-    s_id = id
-    for s in states:
-        if s_id == s.id:
-            check_id = 1
-            s_id = id
-            break
+    amenities = storage.all(Amenity).values()
+    
     return render_template(
-        '9-states.html', states=states, cities=cities,
-        s_id=s_id, check_id=check_id)
-    # Otra forma, capaz que mas facil, es pasar el state correspondiente al id
-    # en lugar del id
-
+        '10-hbnb_filters.html', states=states, cities=cities,
+        amenities=amenities)
 
 @app.teardown_appcontext
 def teardown(error):
